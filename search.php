fe78e3a118a62a8c6760fb2cfd9394b3654a9a38
Will Charczuk <will@clotheshor.se>
2014-02-02 16:49:10 -0500

    search

--- search.php
+++ search.php
@@ -1,29 +1,19 @@
-<?php
-get_header(); 
-?>
-
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-
+<?php get_header(); ?>
+	<div id="main" class="content-area row">
+		<div id="content" class="site-content col-xs-8 col-md-8 col-lg-8" role="main">
 		<?php if ( have_posts() ) : ?>
-
 			<header class="page-header">
 				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentythirteen' ), get_search_query() ); ?></h1>
 			</header>
-
 			<?php /* The loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'content', get_post_format() ); ?>
 			<?php endwhile; ?>
-
-			<?php twentythirteen_paging_nav(); ?>
-
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-		<?php endif; ?>
-
+			<?php chictweak_paging_nav(); ?>
+			<?php else : ?>
+				<?php get_template_part( 'content', 'none' ); ?>
+			<?php endif; ?>
 		</div><!-- #content -->
+		<?php get_sidebar(); ?>
 	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
