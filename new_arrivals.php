0669c430c5173a1b68f783136731d3e066bba01d
Will Charczuk <will@clotheshor.se>
2014-02-10 15:00:07 -0500

    fixing for 5.3

--- new_arrivals.php
+++ new_arrivals.php
@@ -17,8 +17,10 @@ foreach($html->find("div.product") as $node)
 {
 	if($current_items <= $max_items)
 	{
-		$img = $node->find("img")[0];
-		$link = $node->find("a")[0];
+		$imgs = $node->find("img");
+		$img = $imgs[0];
+		$links = $node->find("a");
+		$link = $links[0];
 ?>
 	{ "img_url" : "<?php echo $img->src; ?>", "link_url" : "<?php echo $link->href; ?>" }
 <?php
