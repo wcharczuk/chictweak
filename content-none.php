92a10350caf689d48607a896dfab9c336154efcb
Will Charczuk <will@clotheshor.se>
2014-02-18 16:43:29 -0500

    no results placeholder.

---
+++ content-none.php
@@ -0,0 +1,11 @@
+<article id="post-none" class="post">
+	<header class="entry-header">
+	</header>
+	<div class="entry-summary">
+		No Results!
+	</div>
+	<div class="entry-content entry-content-empty">
+	</div>
+	<footer class="post-social">
+	</footer>
+</article>
--- search.php
+++ search.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
 	<div id="main" class="content-area row">
 		<div id="content" class="site-content col-xs-8 col-md-8 col-lg-8" role="main">
-		<?php if ( have_posts() ) : ?>
 			<header class="page-header">
 				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentythirteen' ), get_search_query() ); ?></h1>
 			</header>
+		<?php if ( have_posts() ) : ?>
 			<?php /* The loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'content', get_post_format() ); ?>
 			<?php endwhile; ?>
 			<?php chictweak_paging_nav(); ?>
-			<?php else : ?>
-				<?php get_template_part( 'content', 'none' ); ?>
-			<?php endif; ?>
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
 		</div><!-- #content -->
 		<?php get_sidebar(); ?>
 	</div><!-- #primary -->
